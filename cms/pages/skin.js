9820cc17ef4bbc47d5fdeb91e735c8013ea868a0
masami <hiraomasami@gmail.com>
2021-05-21 22:22:18 +0900

    editボタン追加

--- cms/pages/skin.js
+++ cms/pages/skin.js
@@ -22,6 +22,7 @@ function Skin({skins}) {
               <td><ColorEditor color={color.code}/></td>
               <td>{color.code}</td>
               <td>
+                <button type="image" className={styles.edit}><Image src="/icon_edit.svg" alt="edit" width={16} height={16}/></button>
                 <button type="image" className={styles.up}><Image src="/icon_up.svg" alt="up" width={14} height={14}/></button>
                 <button type="image" className={styles.down}><Image src="/icon_down.svg" alt="down" width={14} height={14}/></button>
                 <button type="image" className={styles.trash}><Image src="/icon_trash.svg" alt="move to trash" width={14} height={14}/></button>
