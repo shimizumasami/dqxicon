5e95f7053aa09deee504e1b1d65cc2839f3585b5
masami <hiraomasami@gmail.com>
2021-05-24 01:04:11 +0900

    ImageEditor マウスオーバー時のアイコン表示

--- cms/components/imageEditor.js
+++ cms/components/imageEditor.js
@@ -2,10 +2,15 @@ import styles from '../styles/ImageEditor.module.scss'
 
 export default function ImageEditor(props) {
   return (
-    <img
-      src={props.src}
-      alt={props.alt}
-      className={styles.editor}
-    />
+    <div className={styles.container}>
+      <img
+        src={props.src}
+        alt={props.alt}
+        className={styles.editor}
+      />
+      <div className={styles.cover}>
+        <img src="/icon_edit.svg" className={styles.edit} />
+      </div>
+    </div>
   )
 }
