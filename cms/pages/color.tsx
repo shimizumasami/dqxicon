fc2c53701ad48626b54fcd9ffc6b66d7b1b06651
masami <hiraomasami@gmail.com>
2021-06-12 19:06:02 +0900

    color.tsx を関数コンポーネントに書き換え

--- cms/pages/color.tsx
+++ cms/pages/color.tsx
@@ -4,78 +4,68 @@ import TableHead from '../components/tableHead'
 import ColorColumn from '../components/colorColumn'
 import { Color } from '../interfaces/color'
 import styles from '../styles/Main.module.scss'
-import react from 'react'
+import React, { useState } from 'react'
 
 type Props = {
   colors: Color[],  
 }
 
-type State = {
-  colors: Color[],
-}
-
-class ColorPage extends react.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      colors: props.colors
-    }
-  }
+const ColorPage = (props: Props) => {
+  const [colors, setColors] = useState(props.colors)
 
   /**
    * 追加処理.
    */
-  handleCreate() {
-    const colors = this.state.colors.slice()
-    const orders = colors.map(data => {return data.order})
-    colors.push({
+  const handleCreate = () => {
+    const colors_update = colors.slice()
+    const orders = colors_update.map(data => {return data.order})
+    colors_update.push({
       id: Math.random().toString(32).substring(2),
       order: orders.length > 0 ? Math.max(...orders) + 1 : 1,
       name: '新しい色',
       code: '#000000',
       isCreate: true,
     })
-    this.setState({colors: colors})
+    setColors(colors_update)
   }
+
   /**
    * 削除処理.
    * @param id 削除対象のID
    */
-  handleRemove(id: string) {
-    const colors: Color[] = this.state.colors.slice()
-    colors.some((data: Color, index: number) => {
+  const handleRemove = (id: string) => {
+    const colors_update = colors.slice()
+    colors_update.some((data, index) => {
       if (data.id === id) {
-        colors.splice(index, 1)
+        colors_update.splice(index, 1)
       }
     })
-    this.setState({colors: colors})
+    setColors(colors_update)
   }
 
-  render() {
-    return (
-      <App active="color">
-        <Title
-          name="色"
-          onCreate={() => {this.handleCreate()}}
+  return (
+    <App active="color">
+      <Title
+        name="色"
+        onCreate={() => {handleCreate()}}
+      />
+
+      <table className={styles.table}>
+        <TableHead
+          heads={['Order', 'Color', 'Color code', 'Name', 'Action']}
         />
-
-        <table className={styles.table}>
-          <TableHead
-            heads={['Order', 'Color', 'Color code', 'Name', 'Action']}
-          />
-          <tbody>
-            {this.state.colors.map((value) => (
-              <ColorColumn
-                key={value.id}
-                value={value}
-                onRemove={(id: string) => {this.handleRemove(id)}}
-              />
-            ))}
-          </tbody>
-        </table>
-      </App>
-    )
-  }
+        <tbody>
+          {colors.map((value) => (
+            <ColorColumn
+              key={value.id}
+              value={value}
+              onRemove={(id: string) => {handleRemove(id)}}
+            />
+          ))}
+        </tbody>
+      </table>
+    </App>
+  )
 }
 
 export async function getStaticProps() {
